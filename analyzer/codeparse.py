919d5f8e2bae64e16059908b072554c69489b428
spazzoduck <credential.helper>
2012-11-14 15:05:57 -0500

    changed codeparse

--- analyzer/codeparse.py
+++ analyzer/codeparse.py
@@ -2,12 +2,13 @@
 # Finds vulnerabilites in Dalvik bytecode
 
 def get_permission_access(d,dx,permission_names):
-    cm = d.get_class_manager
+    cm = d.get_class_manager()
     permissions = dx.get_permissions(permission_names)
     methods ={}
     for p in permission_names:
         methods[p]=[]
-        for path in permissions[p]:
-            methods[p].append(cm.get_method_ref(path.src_idx))
+        if p in permissions:
+            for path in permissions[p]:
+                methods[p].append(cm.get_method_ref(path.get_src_idx()))
 
-    return methods            
+    return methods
