14a07ac10be7be9373368892872f90147f801470
Kelly Casteel <casteelk@gmail.com>
2012-11-19 13:31:16 -0500

    output permission usage

--- analyzer/codeparse.py
+++ analyzer/codeparse.py
@@ -12,7 +12,8 @@ def get_permission_access(d,dx, perms=[]):
         methods[p]=[]
         for path in permissions[p]:
             try:#if isinstance(path,PathP):
-                methods[p].append(cm.get_method_ref(path.get_src_idx()))
+                bad_thing =cm.get_method_ref(path.get_dst_idx())
+                methods[p].append((cm.get_method_ref(path.get_src_idx()),bad_thing))
             except:# else:
                 print str(p) + " Failed!"
                 pass #methods[p].append(cm.get_method_ref(path.get_dst_idx()))
